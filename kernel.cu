309353f08398f895047e621feff8f50d23181704
maarkristof <maarkristof@gmail.com>
2019-12-12 11:37:11 +0000

    Final program

--- kernel.cu
+++ kernel.cu
@@ -13,34 +13,42 @@
 #include <fstream>
 #include <ctime>
 
-#define CONST_FILE_LENGTH 10000
-#define CONST_WORD_LENGTH_KRISTOF 5
+#define CONST_FILE_LENGTH 20000
+#define CONST_WORD_MAX_LENGTH 12
 
 using namespace std;
 
  /* Global variables */
 uint8_t g_wordLength;
-char g_cracked[CONST_WORD_LENGTH_KRISTOF];
+char g_cracked[CONST_WORD_MAX_LENGTH];
 string g_words[CONST_FILE_LENGTH];
+char g_charArray[CONST_FILE_LENGTH][CONST_WORD_MAX_LENGTH];
 
-__device__ char g_deviceCracked[CONST_WORD_LENGTH_KRISTOF];
-//__device__ string g_deviceWords[CONST_FILE_LENGTH];
+__device__ char g_deviceCracked[CONST_WORD_MAX_LENGTH];
+__device__ char g_deviceCharArray[CONST_FILE_LENGTH][CONST_WORD_MAX_LENGTH];
 
-__global__ void md5Crack(uint8_t wordLength, uint32_t hash01, uint32_t hash02, uint32_t hash03, uint32_t hash04) {
+__global__ void md5Crack(uint32_t hash01, uint32_t hash02, uint32_t hash03, uint32_t hash04) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i < CONST_FILE_LENGTH)
+	{
+		/* Thread variables */
+		uint32_t threadHash01, threadHash02, threadHash03, threadHash04;
+		uint32_t threadWordLength = sizeof(g_deviceCharArray[i]) / sizeof(char);
+		char threadTextWord[CONST_WORD_MAX_LENGTH];
 
-	/* Thread variables */
-	char threadTextWord[6] = "teszt";
-	uint8_t threadWordLength;
-	uint32_t threadHash01, threadHash02, threadHash03, threadHash04;
+		uint32_t charsNum = 0;
+		for (uint32_t j = 0; j < threadWordLength; j++) {
+			if (g_deviceCharArray[i][j] != 0) charsNum++;
+			threadTextWord[j] = g_deviceCharArray[i][j];
+		}
 
-	/* Copy everything to local memory */
-	memcpy(&threadWordLength, &wordLength, sizeof(uint8_t));
+		md5Hash((unsigned char*)threadTextWord, charsNum, &threadHash01, &threadHash02, &threadHash03, &threadHash04);
 
-	md5Hash((unsigned char*)threadTextWord, threadWordLength, &threadHash01, &threadHash02, &threadHash03, &threadHash04);
-
-	if (threadHash01 == hash01 && threadHash02 == hash02 && threadHash03 == hash03 && threadHash04 == hash04) {
-		memcpy(g_deviceCracked, threadTextWord, threadWordLength);
+		if (threadHash01 == hash01 && threadHash02 == hash02 && threadHash03 == hash03 && threadHash04 == hash04) {
+			memcpy(g_deviceCracked, threadTextWord, threadWordLength);
+		}
 	}
+	__syncthreads();
 }
 
 int findHashCPU(string input[CONST_FILE_LENGTH], string inputHash)
@@ -60,15 +68,16 @@ int findHashCPU(string input[CONST_FILE_LENGTH], string inputHash)
 }
 
 int main() {
-	/* password hash to find: cattle*/
-	char passwordHash[33] = "bd5f4b0419caa97dd2f9b4d3238ff92f";
+	/* password hash to find: 06041992, 20000*/
+	char passwordHash[33] = "f439a66cf295371bd7b674ef571e8815";
 
 	/* read text file to array*/
 	string wordsArray[CONST_FILE_LENGTH];
 	ifstream file("passwords.txt");
 	if (file.is_open()) for (int i = 0; i < CONST_FILE_LENGTH; ++i) file >> wordsArray[i];
 
-	/*CPU hash finder*/
+	/*-------------------------------------------------------------------------------------------*/
+	/*CPU PART*/
 	std::clock_t c_start = std::clock();
 
 	int index = findHashCPU(wordsArray, passwordHash);
@@ -77,8 +86,21 @@ int main() {
 
 	double time_elapsed_ms = 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC;
 
-	std::cout << "CPU bruteforce:\n\cracked word: " << wordsArray[index] << std::endl;
+	std::cout << "CPU bruteforce:\n\t\cracked word: " << wordsArray[index] << std::endl;
 	std::cout << "\ttime used: " << time_elapsed_ms << " ms\n";
+
+
+
+	/*-------------------------------------------------------------------------------------------*/
+	/*GPU PART*/
+	/* convert string array to char[][]*/
+	memset(g_charArray, 0, CONST_FILE_LENGTH);
+
+	for (int i = 0; i < CONST_FILE_LENGTH; i++) {
+		for (int j = 0; j < wordsArray[i].length(); j++) {
+			g_charArray[i][j] = wordsArray[i][j];
+		}
+	}
 
 	/* variable for hash stored as u32 integers */
 	uint32_t md5Hash[4];
@@ -92,12 +114,12 @@ int main() {
 	}
 
 	/* fill memory */
-	memset(g_cracked, 0, CONST_WORD_LENGTH_KRISTOF);
-	//memset(g_deviceWords, 0, CONST_FILE_LENGTH);
-	g_wordLength = CONST_WORD_LENGTH_KRISTOF;
+	memset(g_cracked, 0, CONST_WORD_MAX_LENGTH);
+	g_wordLength = CONST_WORD_MAX_LENGTH;
 
 	/* copy to device */
-	cudaMemcpyToSymbol(g_deviceCracked, g_cracked, sizeof(uint8_t) * CONST_WORD_LENGTH_KRISTOF, 0, cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol(g_deviceCracked, g_cracked, sizeof(uint8_t) * CONST_WORD_MAX_LENGTH, 0, cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol(g_deviceCharArray, g_charArray, sizeof(char) * CONST_FILE_LENGTH * CONST_WORD_MAX_LENGTH, 0, cudaMemcpyHostToDevice);
 
 	cudaEvent_t clockBegin;
 	cudaEvent_t clockLast;
@@ -106,10 +128,10 @@ int main() {
 	cudaEventCreate(&clockLast);
 	cudaEventRecord(clockBegin, 0);
 
-	md5Crack <<< 1,1 >>> (g_wordLength, md5Hash[0], md5Hash[1], md5Hash[2], md5Hash[3]);
+	md5Crack <<< 40, 500 >>> (md5Hash[0], md5Hash[1], md5Hash[2], md5Hash[3]);
 
 	/* Copy result */
-	cudaMemcpyFromSymbol(g_cracked, g_deviceCracked, sizeof(uint8_t) * CONST_WORD_LENGTH_KRISTOF, 0, cudaMemcpyDeviceToHost);
+	cudaMemcpyFromSymbol(g_cracked, g_deviceCracked, sizeof(uint8_t) * CONST_WORD_MAX_LENGTH, 0, cudaMemcpyDeviceToHost);
 
 	float milliseconds = 0;
 	cudaEventRecord(clockLast, 0);
